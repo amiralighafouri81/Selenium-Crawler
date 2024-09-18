088c6ed3fbae41cae4286df773ab24b4f6e341c0
amirali ghafouri <amiralighafouri81@gmail.com>
2024-09-18 16:25:07 +0330

    commited version 1.0

--- crawler.py
+++ crawler.py
@@ -1,27 +1,24 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from urllib.parse import urlparse
-import time
-import json
-
 
 class Crawler:
-    def __init__(self, url, max_scrolls=50, pause_time=2, max_links=5, output_file="output.json"):
+    def __init__(self, url, db_collection, max_scrolls=50, pause_time=2, max_links=5):
         self.url = url
         self.max_scrolls = max_scrolls
         self.pause_time = pause_time
         self.max_links = max_links
-        self.output_file = output_file
+        self.db_collection = db_collection
 
         # Set up Chrome options
         options = Options()
         options.add_argument("--headless")
         options.add_experimental_option("detach", True)  # Keep the browser open after the script finishes
 
-        # Initialize the WebDriver
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
     def get_domain(self, url):
@@ -37,13 +34,11 @@ class Crawler:
 
         return main_domain in new_domain
 
-    def scroll_incrementally_and_collect_links(self):
+    def scroll(self):
         all_links = set()  # Use a set to avoid duplicates
         last_height = self.driver.execute_script("return document.body.scrollHeight")
-        main_domain = self.get_domain(self.url)
 
         for _ in range(self.max_scrolls):
-            # Scroll down by a small increment (to trigger lazy loading)
             self.driver.execute_script("window.scrollBy(0, 1000);")
             time.sleep(self.pause_time)  # Wait for content to load
 
@@ -60,11 +55,9 @@ class Crawler:
 
         return all_links
 
-    def crawl_and_extract_data(self, links):
+    def crawl(self, links):
         all_tags = ["p", "h1", "h2", "h3", "h4", "h5", "h6", "span", "div", "img", "form", "input", "textarea"]
         all_attributes = ["src", "href", "alt", "title", "value"]
-
-        data = []
 
         for link in list(links)[:self.max_links]:
             page_data = {"url": link}
@@ -91,24 +84,15 @@ class Crawler:
                                 page_data[tag].append({"type": "attribute", "attribute": attr, "value": value})
 
             except Exception as e:
+                print(f"Error processing {link}: {e}")
                 page_data["error"] = str(e)
 
-            data.append(page_data)
+            self.db_collection.insert_one(page_data)
 
-        # Write the collected data to a JSON file
-        with open(self.output_file, 'w', encoding='utf-8') as f:
-            json.dump(data, f, ensure_ascii=False, indent=4)
-
-        print(f"Data has been saved to {self.output_file}")
+        print("Data inserted into MongoDB")
 
     def run(self):
-        # Navigate to the webpage
         self.driver.get(self.url)
-
-        # Scroll incrementally and collect all links
-        all_links = self.scroll_incrementally_and_collect_links()
-
-        # Crawl and extract data from the first 'max_links' collected links and store it in a JSON file
-        self.crawl_and_extract_data(all_links)
-
+        all_links = self.scroll()
+        self.crawl(all_links)
         self.driver.quit()
--- main.py
+++ main.py
@@ -1,48 +1,27 @@
 from crawler import Crawler
 from pymongo import MongoClient
-import json
-import os
+from concurrent.futures import ThreadPoolExecutor
 
-# Delete output.json if it exists
-if os.path.exists("output.json"):
-    os.remove("output.json")
+def run_crawler(url):
+    scraper = Crawler(url, db_collection=collection, max_scrolls=50, pause_time=5, max_links=5)
+    scraper.run()
 
-# Scrape the website
 urls = []
 with open('urls.txt', 'r') as file:
     for line in file:
         line = line.strip()
-        url = f"{line}"
-        urls.append(url)
+        urls.append(line)
 
-# MongoDB connection settings
 MONGO_URI = "mongodb://localhost:27017/"
-DATABASE_NAME = "Crawler"
+DATABASE_NAME = "Crawler-news"
 COLLECTION_NAME = "news"
-JSON_FILE_PATH = "output.json"
 
-# Connect to MongoDB
 client = MongoClient(MONGO_URI)
 db = client[DATABASE_NAME]
 collection = db[COLLECTION_NAME]
 
-for url in urls:
-    scraper = Crawler(url, max_scrolls=50, pause_time=2, max_links=9, output_file="output.json")
-    scraper.run()
+with ThreadPoolExecutor(max_workers=10) as executor:
+    executor.map(run_crawler, urls)
 
-    # Load data from JSON file
-    with open(JSON_FILE_PATH, 'r', encoding='utf-8') as file:
-        data = json.load(file)
-
-    # Insert data into MongoDB only if it is a non-empty list or dict
-    if data and isinstance(data, list):
-        collection.insert_many(data)
-        print(f"Inserted {len(data)} documents into {DATABASE_NAME}.{COLLECTION_NAME}")
-    elif isinstance(data, dict):
-        collection.insert_one(data)
-        print(f"Inserted 1 document into {DATABASE_NAME}.{COLLECTION_NAME}")
-    else:
-        print(f"No data to insert for {url}")
-
-# Close the connection
 client.close()
+
